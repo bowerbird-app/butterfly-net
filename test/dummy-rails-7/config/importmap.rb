073f63c479cfe04e66f2397920e0ab6b53309647
Lim Choon Yun (Marco) <marco.cylim@gmail.com>
2026-04-29 01:51:35 +0000

    Add FlatPack importmap pins to dummy apps

--- test/dummy-rails-7/config/importmap.rb
+++ test/dummy-rails-7/config/importmap.rb
@@ -6,5 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# FlatPack controllers and TipTap helpers are registered automatically
-# via FlatPack::Engine's importmap initializer (flat_pack/config/importmap.rb).
+# FlatPack controllers, TipTap helpers, and Heroicons.
+pin_all_from File.expand_path("../app/javascript/flat_pack/controllers", __dir__), under: "controllers/flat_pack", to: "flat_pack/controllers", preload: false
+pin_all_from File.expand_path("../app/javascript/flat_pack/tiptap", __dir__), under: "flat_pack/tiptap", to: "flat_pack/tiptap", preload: false
+pin "flat_pack/heroicons", to: "flat_pack/heroicons.js", preload: false
